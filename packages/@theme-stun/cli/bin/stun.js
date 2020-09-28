1c9d72f446c22a853d3b2690b5e9b17d647d5be7
liuyib <1656081615@qq.com>
2020-09-28 11:13:35 +0800

    feat: add validation for node version

--- packages/@theme-stun/cli/bin/stun.js
+++ packages/@theme-stun/cli/bin/stun.js
@@ -1,41 +1,44 @@
 #!/usr/bin/env node
 
-// const { chalk, semver } = require("@theme-stun/cli-utils");
-// const requiredNodeVersion = require("../package.json").engines.node;
+const { chalk, semver } = require('@theme-stun/cli-utils');
+const requiredNodeVersion = require('../package.json').engines.node;
 
-// // Check node version before requiring/doing anything else
-// // The user may be on a very old node version
-// function checkNodeVersion(wanted, name) {
-//   if (semver.satisfies(process.version, wanted, { includePrerelease: true })) {
-//     console.log(
-//       chalk.red(
-//         `You are using Node ${process.version}.\n` +
-//           `${name} requires Node ${wanted}.\nPlease upgrade your Node version.`
-//       )
-//     );
-//     process.exit(1);
-//   }
-// }
+// Check node version before requiring/doing anything else
+// The user may be on a very old node version
+function checkNodeVersion(wanted, name) {
+  if (!semver.satisfies(process.version, wanted, { includePrerelease: true })) {
+    console.log(
+      chalk.red(
+        `You are using Node ${process.version}.\n` +
+          `${name} requires Node${wanted}.\nPlease upgrade your Node version.`,
+      ),
+    );
+    process.exit(1);
+  }
+}
 
-// checkNodeVersion(requiredNodeVersion, "@theme-stun/cli");
+checkNodeVersion(requiredNodeVersion, '@theme-stun/cli');
 
-// const EOL_NODE_MAJORS = ["8.x", "9.x", "11.x", "13.x"];
-// for (const major of EOL_NODE_MAJORS) {
-//   if (semver.satisfies(process.version, major)) {
-//     console.log(
-//       chalk.red(
-//         `You are using Node ${process.version}.\n` +
-//           `Node.js ${major} has already reached end-of-life and will not be supported in future major releases.\n` +
-//           `It's strongly recommended to use an active LTS version instead.`
-//       )
-//     );
-//   }
-// }
+const EOL_NODE_MAJORS = ['8.x', '9.x', '11.x', '13.x'];
+for (const major of EOL_NODE_MAJORS) {
+  if (semver.satisfies(process.version, major)) {
+    console.log(
+      chalk.red(
+        `You are using Node ${process.version}.\n` +
+          `Node.js ${major} has already reached end-of-life and will not be supported in future major releases.\n` +
+          `It's strongly recommended to use an active LTS version instead.`,
+      ),
+    );
+  }
+}
 
 const program = require('commander');
 
 program
-  .version(`@theme-stun/cli ${require('../package.json').version}`, '-v, --version')
+  .version(
+    `@theme-stun/cli ${require('../package.json').version}`,
+    '-v, --version',
+  )
   .usage('<command> [options]');
 
 // program
